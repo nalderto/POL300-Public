5af595da0b53198c8051a0d67647997926e1f07f
Noah Alderton <nalderto@purdue.edu>
2020-08-25 23:11:59 -0400

    Update anaconda-install.sh

--- anaconda-install.sh
+++ anaconda-install.sh
@@ -3,6 +3,6 @@
 module load anaconda
 rcac-conda-env create -n POL300 -j -y
 module load use.own
-module load conda-env/POL300-py3.7.0
+module load conda-env/POL300-py3.6.4
 pip install textblob textstat summa pandas plotly requests bs4
 exit
