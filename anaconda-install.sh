5eee284955993cfb027fecff3a9864d2f041d17c
Noah Alderton <nalderto@purdue.edu>
2020-09-06 15:26:22 -0400

    Update anaconda-install.sh

--- anaconda-install.sh
+++ anaconda-install.sh
@@ -4,5 +4,5 @@ module load anaconda
 rcac-conda-env create -n POL300 -j -y
 module load use.own
 module load conda-env/POL300-py3.6.4
-pip install textblob textstat summa pandas plotly requests bs4
+pip install textblob textstat summa pandas plotly requests bs4 lxml
 echo -e "\nDone!  You can now close this tab and return to your Jupyter notebook.  Make sure you refresh the Jupyter page so the POL 300 Python environment shows up."
