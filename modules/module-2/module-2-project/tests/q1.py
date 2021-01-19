ad9086767d0afe333dfd7a3dce0fd2c8ed63513b
Noah Alderton <noahlouisalderton@gmail.com>
2021-01-19 05:01:36 -0500

    Change 2016 to 2018

--- modules/module-2/module-2-project/tests/q1.py
+++ modules/module-2/module-2-project/tests/q1.py
@@ -4,7 +4,7 @@ test = {'name': 'q1',
                                         >>> import requests, csv 
                                         >>> response = requests.get("https://www.brookings.edu/wp-content/uploads/2017/01/vitalstats_ch3_tbl1.csv").content.decode("ascii") 
                                         >>> reader = list(csv.DictReader(response.splitlines())) 
-                                        >>> assert find_min_2016_dollars(reader) == {"Year": "1990", "Chamber": "House", "NominalDollars": "423245", "2016Dollars": "813158"}""",
+                                        >>> assert find_min_2016_dollars(reader) == {"Year": "1990", "Chamber": "House", "NominalDollars": "423245", "2018Dollars": "813158"}""",
                                'hidden': False, 'locked': False}],
                     'scored': True,
                     'setup': '',
