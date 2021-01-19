75dbb164256a98a57071c61b13eccd619fe47ea6
Noah Alderton <noahlouisalderton@gmail.com>
2021-01-19 04:58:19 -0500

    Updated mod 2 proj tests

--- modules/module-2/module-2-project/tests/q1.py
+++ modules/module-2/module-2-project/tests/q1.py
@@ -1,6 +1,10 @@
 test = {'name': 'q1',
         'points': 1,
-        'suites': [{'cases': [{'code': 'import requests, csv >>> response = requests.get("https://www.brookings.edu/wp-content/uploads/2017/01/vitalstats_ch3_tbl1.csv").content.decode("ascii") >>> reader = list(csv.DictReader(response.splitlines())) >>> assert find_min_2016_dollars(reader) == {"Year": "1990", "Chamber": "House", "NominalDollars": "423245", "2016Dollars": "777213"}',
+        'suites': [{'cases': [{'code': """
+                                        >>> import requests, csv 
+                                        >>> response = requests.get("https://www.brookings.edu/wp-content/uploads/2017/01/vitalstats_ch3_tbl1.csv").content.decode("ascii") 
+                                        >>> reader = list(csv.DictReader(response.splitlines())) 
+                                        >>> assert find_min_2016_dollars(reader) == {"Year": "1990", "Chamber": "House", "NominalDollars": "423245", "2016Dollars": "813158"}""",
                                'hidden': False, 'locked': False}],
                     'scored': True,
                     'setup': '',
--- modules/module-2/module-2-project/tests/q3.py
+++ modules/module-2/module-2-project/tests/q3.py
@@ -1,6 +1,10 @@
 test = {'name': 'q3',
         'points': 1,
-        'suites': [{'cases': [{'code': 'import requests, csv >>> response = requests.get("https://www.brookings.edu/wp-content/uploads/2017/01/vitalstats_ch3_tbl1.csv").content.decode("ascii") >>> reader = list(csv.DictReader(response.splitlines())) >>> assert find_bigger_spender(reader) == ("Senate", 8111022.6875, 6908696.3125)',
+        'suites': [{'cases': [{'code': """
+                                        >>> import requests, csv 
+                                        >>> response = requests.get("https://www.brookings.edu/wp-content/uploads/2017/01/vitalstats_ch3_tbl1.csv").content.decode("ascii") 
+                                        >>> reader = list(csv.DictReader(response.splitlines())) 
+                                        >>> assert find_bigger_spender(reader) == ("Senate", 8861261.059823528, 7554219.041499998)""",
                                'hidden': False, 'locked': False}],
                     'scored': True,
                     'setup': '',
