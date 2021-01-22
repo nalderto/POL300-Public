2214c2951aa10645879fc0ae2c3bda29b720db88
Noah Alderton <noahlouisalderton@gmail.com>
2021-01-22 07:22:49 -0500

    Added Google Colab guide PDF

---
+++ modules/module-4/module-4-project/tests/q1.py
@@ -0,0 +1,31 @@
+test = {
+    "name": "q1",       # name of the test
+    "points": 1,        # number of points for the entire suite
+    "suites": [         # list of suites, only 1 suite allowed!
+        {
+            "cases": [                  # list of test cases
+                {
+                    "code": r"""
+                    >>> import pandas as pd
+                    >>> state = get_state()
+                    >>> chamber = get_chamber()
+                    >>> state = state.title()
+                    >>> chamber = chamber.title().replace(" Of ", " of ")
+                    >>> answer_df = pd.read_csv("https://raw.githubusercontent.com/nalderto/POL300-Public/master/All-Campaign-Contributions.csv")
+                    >>> answer_df = answer_df[(answer_df['State'] == state) & (answer_df['Chamber'] == chamber)].reset_index(drop=True).sort_index(axis=1)
+                    >>> filename = state.replace(" ", "-") + "-" + chamber.replace(" ", "-") + "-Campaign-Contributions.csv"
+                    >>> collectCampaignContributions()
+                    >>> test_df = pd.read_csv(filename).reset_index(drop=True).sort_index(axis=1)
+                    >>> assert test_df.equals(answer_df)
+                    """,
+                    "hidden": False,
+                    "locked": False,
+                }, 
+            ],
+            "scored": False,            # ignored by Otter
+            "setup": "",                # ignored by Otter
+            "teardown": "",             # ignored by Otter
+            "type": "doctest"           # the type of test; only "doctest" allowed
+        },
+    ]
+}
