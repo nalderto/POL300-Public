f62742f76e31c1d12d67b0183e9759ef5bf161dd
Noah Alderton <noahlouisalderton@gmail.com>
2021-01-22 10:00:10 -0500

    Fixes mod 4 proj test cases

--- modules/module-4/module-4-project/tests/q1.py
+++ modules/module-4/module-4-project/tests/q1.py
@@ -6,6 +6,11 @@ test = {
             "cases": [                  # list of test cases
                 {
                     "code": r"""
+                    >>> import sys, os
+		    >>> def blockPrint():
+		    ...		sys.stdout = open(os.devnull, 'w')
+		    >>> def enablePrint():
+		    ...		sys.stdout = sys.__stdout__
                     >>> import pandas as pd
                     >>> state = get_state()
                     >>> chamber = get_chamber()
@@ -14,7 +19,9 @@ test = {
                     >>> answer_df = pd.read_csv("https://raw.githubusercontent.com/nalderto/POL300-Public/master/All-Campaign-Contributions.csv")
                     >>> answer_df = answer_df[(answer_df['State'] == state) & (answer_df['Chamber'] == chamber)].reset_index(drop=True).sort_index(axis=1)
                     >>> filename = state.replace(" ", "-") + "-" + chamber.replace(" ", "-") + "-Campaign-Contributions.csv"
+                    >>> blockPrint()
                     >>> collectCampaignContributions()
+                    >>> enablePrint()
                     >>> test_df = pd.read_csv(filename).reset_index(drop=True).sort_index(axis=1)
                     >>> assert test_df.equals(answer_df)
                     """,
